1a08fa61b885bcc89edf002968b1c8206ce27ddf
Pritam Khan <96115217+PritamKhan@users.noreply.github.com>
2023-07-19 21:52:43 +0530

    Add files via upload

---
+++ Life in Weeks.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+age = int(input("What is your current age? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+remining_age = 90 - age
+days_left = remining_age * 365
+weeks_left = remining_age * 52
+month_left = remining_age * 12
+
+print(f"You have {days_left} days, {weeks_left} weeks, and {month_left} months left.")
+
