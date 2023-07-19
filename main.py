f1cf312da8813fe34337836d061ea1c76ad37909
Pritam Khan <96115217+PritamKhan@users.noreply.github.com>
2023-07-19 21:47:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+
+print("Welcome to the brand  name generator. ")
+city=input("Which city you are grow up in?\n")
+pet=input("what is the name of a pet?\n")
+print("Your brand name could be "+ city + " " + pet)
